0c96e8dfcec4f8d13e6385b93e23116f21108573
GUBONWOO <zcwxzsx@gmail.com>
2025-03-05 15:12:12 +0900

    fireconsole

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// src/firebaseConfig.js
+// (웹에서만 필요)
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+// Firebase 초기화
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
