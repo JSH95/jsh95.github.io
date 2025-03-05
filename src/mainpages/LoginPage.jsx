966a8f0ca6efc8d66c021b893377951ef1c3ebd8
GUBONWOO <zcwxzsx@gmail.com>
2025-03-05 09:35:23 +0900

    add notification for app

--- src/mainpages/LoginPage.jsx
+++ src/mainpages/LoginPage.jsx
@@ -1,55 +1,74 @@
-import React, {useEffect, useState} from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../config/AuthContext"; // AuthContext에서 useAuth 훅 가져오기
-import createAxiosInstance from "../config/api";
-import "../cssFiles/Login.css";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../config/AuthContext'; // AuthContext에서 useAuth 훅 가져오기
+import createAxiosInstance from '../config/api';
+import '../cssFiles/Login.css';
 import containerImage from '../images/loginpageBackground.png';
 import logoImage from '../images/loginpageLogo.png';
+import { PushNotifications } from '@capacitor/push-notifications'; // 알림 API 추가
 
 function LoginPage() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const { login } = useAuth(); // AuthContext에서 setIsLoggedIn 가져오기
   const [remember, setRemember] = useState(false);
 
   useEffect(() => {
-    const savedUsername = localStorage.getItem("savedUsername");
+    const savedUsername = localStorage.getItem('savedUsername');
     if (savedUsername) {
       setUsername(savedUsername);
       setRemember(true);
     }
   }, []);
 
+  useEffect(() => {
+    // 알림 권한 요청
+    const requestNotificationPermission = async () => {
+      const status = await PushNotifications.requestPermissions();
+      if (status.receive === 'granted') {
+        console.log('알림 권한이 허용되었습니다.');
+      } else {
+        console.log('알림 권한이 거부되었습니다.');
+      }
+    };
+
+    requestNotificationPermission();
+  }, []);
+
   const handleLogin = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-    setError("");
+    setError('');
     if (remember) {
-      localStorage.setItem("savedUsername", username);
+      localStorage.setItem('savedUsername', username);
     } else {
-      localStorage.removeItem("savedUsername");
+      localStorage.removeItem('savedUsername');
     }
     try {
       const axiosInstance = createAxiosInstance(); // 인스턴스 생성
-      const response = await axiosInstance.post("/login", {
+      const response = await axiosInstance.post('/login', {
         username,
         password,
       });
-      const { success, message, token, role, username: responseUsername} = response.data;
+      const {
+        success,
+        message,
+        token,
+        role,
+        username: responseUsername,
+      } = response.data;
       if (success) {
-        // console.log("로그인 성공:", message);
         login(token, role, responseUsername); // 수정된 변수명 적용
-        // navigate(role === "ROLE_ADMIN" ? "/dashboard" : "/workSchedule/list");
-        if(role === "ROLE_ADMIN") {
-          navigate("/dashboard");
+        if (role === 'ROLE_ADMIN') {
+          navigate('/dashboard');
         } else {
-          navigate("/workSchedule/main");
+          navigate('/workSchedule/main');
         }
       } else {
-        setError(message || "알 수 없는 오류가 발생했습니다.");
+        setError(message || '알 수 없는 오류가 발생했습니다.');
       }
     } catch (error) {
       if (
@@ -59,80 +78,84 @@ function LoginPage() {
       ) {
         setError(error.response.data.message);
       } else {
-        setError("로그인에 실패했습니다. 다시 시도하세요.");
+        setError('로그인에 실패했습니다. 다시 시도하세요.');
       }
     } finally {
       setIsLoading(false); // 로딩 상태 비활성화
     }
   };
 
-
-
   return (
-        <div className="login_container" style={{ backgroundImage: `url(${containerImage})` }}>
-          <div className="content">
-            <form onSubmit={handleLogin} className="login-box">
-              {/* 로고 이미지 */}
-              <div className="logo" style={{
-                width: "", // 크기 지정
-                height: "200px",
-                backgroundImage: `url(${logoImage})`,
-                backgroundSize: "contain", // 크기 조정
-                backgroundRepeat: "no-repeat", // 반복 방지
-                backgroundPosition: "center", // 중앙 정렬
-              }}/>
+    <div
+      className='login_container'
+      style={{ backgroundImage: `url(${containerImage})` }}
+    >
+      <div className='content'>
+        <form onSubmit={handleLogin} className='login-box'>
+          <div
+            className='logo'
+            style={{
+              width: '', // 크기 지정
+              height: '200px',
+              backgroundImage: `url(${logoImage})`,
+              backgroundSize: 'contain', // 크기 조정
+              backgroundRepeat: 'no-repeat', // 반복 방지
+              backgroundPosition: 'center', // 중앙 정렬
+            }}
+          />
 
-              {/* 로그인 타이틀 */}
-              <div className="login-title">Login</div>
+          <div className='login-title'>Login</div>
 
-              {/* 입력 필드 */}
-              <div className="input-container">
-                <div className="input-label">User ID</div>
-                <div className="input-box">
-                  <input type="text"
-                         id="username"
-                         value={username}
-                         onChange={(e) => setUsername(e.target.value)}
-                         required
-                         autoComplete="current-password"
-                         placeholder="Enter your ID"
-                         className="input-field"
-                  />
-                </div>
-              </div>
-
-              <div className="input-container">
-                <div className="input-label">Password</div>
-                <div className="input-box">
-                  <input type="password"
-                         id="password"
-                         value={password}
-                         onChange={(e) => setPassword(e.target.value)}
-                         required
-                         autoComplete="current-password"
-                         placeholder="Enter your password"
-                         className="input-field" />
-                </div>
-              </div>
-
-              {/* 아이디 저장 체크박스 */}
-              <div className="checkbox-container">
-                <label className="checkbox-label">
-                  <input
-                      type="checkbox"
-                      checked={remember}
-                      onChange={() => setRemember(!remember)}
-                  />{" "} 아이디 저장
-                </label>
-              </div>
-
-              {/* 로그인 버튼 */}
-              <button type="submit" className="login-button" disabled={isLoading}>
-                {isLoading ? "Loading..." : "로그인"}</button>
-              {error && <p className="error-message">{error}</p>}
-            </form>
+          <div className='input-container'>
+            <div className='input-label'>User ID</div>
+            <div className='input-box'>
+              <input
+                type='text'
+                id='username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                autoComplete='current-password'
+                placeholder='Enter your ID'
+                className='input-field'
+              />
+            </div>
           </div>
-        </div>
+
+          <div className='input-container'>
+            <div className='input-label'>Password</div>
+            <div className='input-box'>
+              <input
+                type='password'
+                id='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                autoComplete='current-password'
+                placeholder='Enter your password'
+                className='input-field'
+              />
+            </div>
+          </div>
+
+          <div className='checkbox-container'>
+            <label className='checkbox-label'>
+              <input
+                type='checkbox'
+                checked={remember}
+                onChange={() => setRemember(!remember)}
+              />{' '}
+              아이디 저장
+            </label>
+          </div>
+
+          <button type='submit' className='login-button' disabled={isLoading}>
+            {isLoading ? 'Loading...' : '로그인'}
+          </button>
+          {error && <p className='error-message'>{error}</p>}
+        </form>
+      </div>
+    </div>
   );
 }
 
